267dca5f363000fb8d131ecd316b5faa2985fc2e
haimu99 <43139675+haimu99@users.noreply.github.com>
2018-10-21 18:08:33 +0700

    cm

---
+++ Dictionary3/src/Word.java
@@ -0,0 +1,37 @@
+public class Word {
+    private String word_target;                     
+    private String word_explain;                     
+
+   
+
+    public Word() {}                                                               
+
+    public Word(String word_target, String word_explain) {                          
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
+    public Word(String lineInFile){                                                
+        this.word_target = lineInFile. substring(0, lineInFile.indexOf("\t"));
+        this.word_explain = lineInFile. substring(lineInFile.indexOf("\t") + 1);
+    }
+
+
+    public String getWord_target() {
+        return word_target;
+    }
+
+    public void setWord_target(String word_target) {
+        this.word_target = word_target;
+    }
+
+    public String getWord_explain() {
+        return word_explain;
+    }
+
+    public void setWord_explain(String word_explain) {
+        this.word_explain = word_explain;
+    }
+
+}
+
